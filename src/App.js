fb2300f946134e07dc28ea2cde27ba343d6fdc2d
WP Myanmar <staffcondoria@gmail.com>
2021-09-19 08:54:21 +0630

    Post data to Google Sheet with ReactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, Form, Container, Header } from 'semantic-ui-react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [sheetConnect, setSheetConnect] = useState('');
+	const [name, setName] = useState('');
+	const [genre, setGenre] = useState('');
+	const [rating, setRating] = useState('');
+	const [synopsis, setSynopsis] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+    const objt = { sheetConnect, name, genre, rating, synopsis };
+
+		axios
+			.post(
+				sheetConnect,
+				objt
+			)
+			.then((response) => {
+				console.log(response);
+			});
+	};
+
+	return (
+		<Container fluid className="container">
+			<Header as="h2">Post to Movie API Database</Header>
+			<Form className="form">
+        <Form.Field>
+					<label>Sheet.best URL</label>
+					<input
+						placeholder="Enter The Link from Sheet.best"
+						onChange={(e) => setSheetConnect(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>Name</label>
+					<input
+						placeholder="Enter Movie Name"
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>Genre</label>
+					<input
+						placeholder="Enter Movie Genre"
+						onChange={(e) => setGenre(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>IMDB Rating</label>
+					<input
+						placeholder="Enter IMDB Rating"
+						onChange={(e) => setRating(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>Synopsis</label>
+					<input
+						placeholder="Enter BaNyar TharaKar"
+						onChange={(e) => setSynopsis(e.target.value)}
+					/>
+				</Form.Field>
+
+				<Button color="blue" type="submit" onClick={handleSubmit}>
+					Submit
+				</Button>
+			</Form>
+			<p>&copy;2021 StaffCondor | Hosted on Git and Deploy with Pages.Dev</p>
+		</Container>
+	);
 }
 
 export default App;
